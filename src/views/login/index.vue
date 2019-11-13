8129e9f11f15250c5f4e68a8a62d72c1b3a3daba
honghonger <1121101850@qq.com>
2019-11-13 11:50:23 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,19 +6,22 @@
 <div class="title">
   <img src="../../assets/img/logo_index.png" alt="">
 </div>
-<el-form style="margin-top:30px">
-  <el-form-item>
-    <el-input></el-input>
+<!-- 表单 -->
+
+<el-form ref="formObj" style="margin-top:30px" :model="loginForm" :rules="loginRules">
+  <!-- 一个表单域就是一个form-item -->
+  <el-form-item prop="mobile">
+    <el-input v-model="loginForm.mobile" placeholder="请输入您的手机号"></el-input>
   </el-form-item>
-  <el-form-item>
-     <el-input style="width:280px"></el-input>
+  <el-form-item prop="code">
+     <el-input v-model="loginForm.code" placeholder="请输入您的验证码" style="width:280px"></el-input>
      <el-button style="float:right" plain>发送验证码</el-button>
   </el-form-item>
-  <el-form-item>
-<el-checkbox>我已阅读并同意用户协议及条款</el-checkbox>
+  <el-form-item prop="checked">
+<el-checkbox v-model="loginForm.checked">我已阅读并同意用户协议及条款</el-checkbox>
   </el-form-item>
   <el-form-item>
-<el-button style="width:100%" type="primary">登录</el-button>
+<el-button @click="login" style="width:100%" type="primary">登录</el-button>
   </el-form-item>
 </el-form>
 </el-card>
@@ -28,7 +31,48 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        checked: false
+      },
+      loginRules: {
+        // 校验规则对象
+        mobile: [
+          {
+            required: true,
+            message: '请输入您的手机号'
+          },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }
+        ],
+        code: [{ required: true,
+          message: '请输入您的验证码' },
+        { pattern: /^\d{6}$/, message: '验证码为6位数字' }
+        ],
+        checked: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('您需要勾选用户协议'))
+          }
+        } }]
+        // rule当前的规则
+        // value代表当前的值 checked的值
+        // callback回调函数
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.formObj.validate(function (isok) {
+        if (isok) {
+          // 如果成功，调用接口登录
+        }
+      })
+    }
+  }
 }
 </script>
 
