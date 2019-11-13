a1f3aa51cfabfc900850ee824a3813e58e32de71
honghonger <1121101850@qq.com>
2019-11-13 09:23:09 +0800

    黑马头条pc-初始化项目-页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home/index.vue'
+import Login from '../views/login'
 Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  name: 'home',
-  component: Home
-}
+const routes = [
+
+  {
+    path: '/',
+    redirect: '/home'
+  }, {
+    path: '/home',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/login',
+    component: Login
+  }
   // {
   //   path: '/about',
   //   name: 'about',
